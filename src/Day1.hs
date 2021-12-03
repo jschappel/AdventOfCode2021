3f4ab50e033af59dad50c1b3b43a1f58ed04b832
jschappel <jmschappel12@gmail.com>
2021-12-02 20:39:13 -0500

    Finished day1

---
+++ src/Day1.hs
@@ -0,0 +1,24 @@
+module Day1 where
+
+import Data.List as List
+
+filepath = "files/depth.txt"
+
+
+sumTriples :: (Num a) => [a] -> [a]
+sumTriples (x:y:z:xs) = (x + y + z):sumTriples (y:z:xs)
+sumTriples _ = []
+
+
+sumDepths = do
+  ls <- fmap lines (readFile filepath)
+  let f:rest = map (\x -> read x :: Integer) ls
+      num = sum . snd . List.mapAccumL (\a x -> (x, if x > a then 1 else 0)) f $ rest
+  putStrLn $ show num
+
+
+sumDepthsAsTriples = do
+  ls <- fmap lines (readFile filepath)
+  let f:rest = sumTriples (map (\x -> read x :: Integer) $ ls)
+      num = sum . snd . List.mapAccumL (\a x -> (x, if x > a then 1 else 0)) f $ rest
+  putStrLn $ show num
